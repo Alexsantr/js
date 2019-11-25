ddab8330dae895fcdbffb0f6ce49f6f501d7da64
Alexsantr <50623230+Alexsantr@users.noreply.github.com>
2019-11-25 09:16:14 +0300

    Update js.js

--- js/js.js
+++ js/js.js
@@ -1,25 +1,23 @@
 //1. Написать функцию, которая на вход принимает целое число и возвращает факториал
 //этого числа.
-function factorial(n) {
-    let fact = 1;
-    for(let i=1; i<=n; i++){
-      fact=fact*i;
-    }
-    return fact;
+   function factorial(n) {
+    return (n != 1) ? n * factorial(n - 1) : 1;
   }
   
-  
-  console.log("1)");
-  console.log(factorial(5));
+  console.log( factorial(7) ); 
+
   
   //2. Написать функцию, которая на вход принимает строку и возвращает длину самого
   //длинного слова в этой строке.
-  function longestWord(str){
-    return Math.max.apply(null, str.split(" ").map(word => word.length));
+  function findLongestWord(str) {
+    var longestWord = str.split(' ').reduce(function(longest, currentWord) {
+      return currentWord.length > longest.length ? currentWord : longest;
+    }, "");
+    return longestWord.length;
   }
-  
   console.log("2)");
-  console.log(longestWord(""));
+  console.log(findLongestWord("Therrr quickrrr brown foxxxx jumpedууу over the lazyyyy dog")); 
+   
   
   //3. Написать функцию, которая на вход принимает массив, состоящий из массивов
   //целых чисел (например: [[1, 2, 3], [4, 5], [6, 7, 8, 9]]) и возвращает массив
@@ -29,18 +27,18 @@ function factorial(n) {
   }
   
   console.log("3)");
-  console.log(arrayOfLargestNum([[1, 2, 3], [4, 5], [6, 7, 8, 9]]));
   
+  console.log(arrayOfLargestNum([[1, 2, 3], [4, 5], [6, 7, 8, 9]]));
+
   //4. Написать функцию, которая на вход принимает строку и целое число
   //и обрезает строку (первый аргумент), если она длиннее заданной
   //максимальной длины строки (второй аргумент) и возвращает обрезанную строку с «…»
   //в конце.
-  function shortWord(str, num){
-    return str.length > num? str.slice(0, num-1) + "…": str;
+  function shortWord(str, number){
+    return str.length > number? str.slice(0, number-1) + "....": str;
   }
-  
   console.log("4)");
-  console.log(shortWord("пример", 4));
+  console.log(shortWord("пример", 5));
   
   //5. Написать функцию, которая на вход принимает строку и возвращает эту строку в
   //формате: каждое слово начинается с заглавной буквы, остальные в нижнем регистре.
@@ -56,23 +54,28 @@ function factorial(n) {
   //индекса n второго массива и вернуть полученный массив. Входные массивы не должны
   //изменяться. (например: при входных данных ([1, 2, 3], [4, 5], 1) должно вернуться
   // [4, 1, 2, 3, 5].
-  function copyArray(mas1, mas2, n){
+  function copyArray(m1, m2, n){
     arr=[];
-    return arr.concat(mas2.slice(0, n), mas1, mas2.slice(n, mas2.length));
+    return arr.concat(m2.slice(0, n), m1, m2.slice(n, m2.length));
   }
   
   console.log("6)");
-  console.log(copyArray([1, 2, 2], [4, 5], 1));
+  console.log(copyArray([1, 2, 3], [4, 5], 1));
   
   //7. Написать функцию, которая на вход принимает массив и удаляет в нем все "ложные
   //значения"
   
-  function arrayWithoutFalsy(mas){
-    return mas.filter(Boolean);
-  }
-  
-  console.log("7)");
-  console.log(arrayWithoutFalsy([1, 0, "", "", false, NaN, "" ,undefined, 43, "", "", 0, 1, 5]));
+    function trueData(m){return m.filter(Boolean);
+   }
+   console.log("7)");
+   console.log(trueData([232, '', null, 'JavaScript', undefined, 0]));
+
+
+
+//    var strs = ['Hello', ',', 'JavaScript', 'World', '!'];
+// var data = filter(strs, function(str) {
+//   return str.toLowerCase() !== str;
+// });
   
   //8. Написать функцию, которая на вход принимает массив, состоящий из двух строк.
   //Функция должна вернуть true, если строка в первом элементе массива содержит все
@@ -83,14 +86,14 @@ function factorial(n) {
   }
   
   console.log("8)");
-  console.log(stringLetters(["кееееsdfееек", "LAккккеееееdsfеееееg"]));
+  console.log(stringLetters(["кееееsdfееек", "кееееsdfееек"]));
   
   //9. Написать функцию которая на вход принимает массив и целое число. Функция должна
   //разбить массив (первый аргумент) на группы длиной size (второй агрумент) и вернуть их
   //в виде двумерного массива.
-  function splitArray(mas, size){
-    while(mas.length)
-      arr.push(mas.splice(0, size));
+  function splitArray(mass, size){
+    while(mass.length)
+      arr.push(mass.splice(0, size));
     return arr;
   }
   
@@ -99,12 +102,12 @@ function factorial(n) {
   
   //10. Используя рекурсию (без использования циклов). Написать функцию, которая
   //принимает массив и целое число (n) и заполняет массив числами от n до 1.
-  function fillArray(mas, n){
+  function element(mas, n){
     mas.push(n);
-    if (n>1) fillArray(mas, n-1);
+    if (n>1) element(mas, n-1);
   }
   
   console.log("10)");
   massiv = [];
-  fillArray(massiv, 15);
+  element(massiv,1);
   console.log(massiv);
